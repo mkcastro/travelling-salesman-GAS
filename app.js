c6787f315bc736a20836c084d0377d625fb0884d
Mike Castro <mkcastro24@gmail.com>
2020-12-17 17:03:10 +0800

    bugfix

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ function optimal_route(stops, startAddress, endAddress) {
     directionFinder.setOptimizeWaypoints(true);
 
     // 4. Adding addresses to the route
-    stops.forEach((stop) => {
-        let address = stop[0];
+    for (let i = 0; i < stops.length; i++) {
+        let address = stops[i][0];
         if (address.length > 0) {
             directionFinder.addWaypoint(address);
         }
-    });
+    }
 
     // 5. Compute optimal route
     const directions = directionFinder.getDirections();
@@ -24,9 +24,10 @@ function optimal_route(stops, startAddress, endAddress) {
 
     // 6. Assign the stop position to each address
     const stopSequence = [["Stop #"]];
-    stopsOrder.forEach(stop => {
-        stopSequence.push([stop + 1]);
-    });
+    for (let j = 0; j < stopsOrder.length; j++) {
+        let stop = stopsOrder.indexOf(j) + 1;
+        stopSequence.push([stop]);
+    }
 
     // 7. Return the result
     return stopSequence;
